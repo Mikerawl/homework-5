2407d2bcad91ca77ae384b329f79f2785e527336
Michael Rawlings <rawlingsm99@gmail.com>
2022-01-03 17:46:16 -0600

    trying to fix some mistakes I made

---
+++ Task List_files/app.js
@@ -0,0 +1,14 @@
+let val;
+
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
+
+val = listItem;
+val = list;
+
+val = list.childNodes;
+val - list.childNodes[0];
+
+// val = list.children;
+ 
+console.log(val)
